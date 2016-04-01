50e817e83f49563956d6053a2fb96a9119402d9c
Terry Lyons <lyonst@users.noreply.github.com>
2016-04-01 12:15:42 -0400

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace sensorupdate
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        /// <summary>
+        /// a the
+        /// </summary>
+        private static void Run()
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri("http://165.88.202.71/");
+                    httpClient.DefaultRequestHeaders.Accept.Clear();
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    bool running = false;
+
+                    do
+                    {
+                        var response =httpClient.PostAsJsonAsync("sensors/laundry/0", running.ToString()).Result;
+                        response = httpClient.PostAsJsonAsync("sensors/laundry/1", running.ToString()).Result;
+
+                        Thread.Sleep(TimeSpan.FromMinutes(5));
+
+                        running = !running;
+                    }
+                    while (!Console.KeyAvailable);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
